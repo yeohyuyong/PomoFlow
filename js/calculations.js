e54150b7e95b145724cf49b5bbe1a2a4b5ff4acf
Yeoh Yu Yong <yeoh.yuyong@gmail.com>
2022-05-21 15:24:07 +0800

    fix error

--- js/calculations.js
+++ js/calculations.js
@@ -1,10 +1,12 @@
+const secondsUp = document.getElementById('secondsUp');
+const minutesUp = document.getElementById('minutesUp');
+
 function displayMinutesOrSeconds(time) {
 	return time <= 9 ? '0' + time : time;
 }
 
 function displayTime(minutes, seconds) {
 	minutesUp.innerHTML = minutes <= 9 ? '0' + minutes : minutes;
-	console.log(minutesUp);
 	secondsUp.innerHTML = seconds <= 9 ? '0' + seconds : seconds;
 }
 
